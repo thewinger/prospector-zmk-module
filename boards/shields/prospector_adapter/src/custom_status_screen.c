7e0a168a0ac439edbc770450053cd3880da7b1ed
thewinger <winger.87@gmail.com>
2025-04-01 20:49:07 +0200

    Battery to the top

--- boards/shields/prospector_adapter/src/custom_status_screen.c
+++ boards/shields/prospector_adapter/src/custom_status_screen.c
@@ -1,11 +1,11 @@
 #include <lvgl.h>
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
+#include <zephyr/kernel.h>
 
-#include "widgets/layer_roller.h"
 #include "widgets/battery_bar.h"
 #include "widgets/caps_word_indicator.h"
+#include "widgets/layer_roller.h"
 
 #include <fonts.h>
 #include <sf_symbols.h>
@@ -20,26 +20,30 @@ static struct zmk_widget_battery_bar battery_bar_widget;
 static struct zmk_widget_caps_word_indicator caps_word_indicator_widget;
 
 lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen;
-    screen = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
+  lv_obj_t *screen;
+  screen = lv_obj_create(NULL);
+  lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
+  lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
 
 #ifdef CONFIG_DT_HAS_ZMK_BEHAVIOR_CAPS_WORD_ENABLED
-    zmk_widget_caps_word_indicator_init(&caps_word_indicator_widget, screen);
-    lv_obj_align(zmk_widget_caps_word_indicator_obj(&caps_word_indicator_widget), LV_ALIGN_RIGHT_MID, -10, 46);
+  zmk_widget_caps_word_indicator_init(&caps_word_indicator_widget, screen);
+  lv_obj_align(zmk_widget_caps_word_indicator_obj(&caps_word_indicator_widget),
+               LV_ALIGN_RIGHT_BOTTOM, 0, 0);
 #endif
 
-    zmk_widget_battery_bar_init(&battery_bar_widget, screen);
-    // lv_obj_set_width(zmk_widget_battery_bar_obj(&battery_bar_widget), lv_pct(100));
-    lv_obj_set_size(zmk_widget_battery_bar_obj(&battery_bar_widget), lv_pct(100), 48);
-    lv_obj_align(zmk_widget_battery_bar_obj(&battery_bar_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
+  zmk_widget_battery_bar_init(&battery_bar_widget, screen);
+  // lv_obj_set_width(zmk_widget_battery_bar_obj(&battery_bar_widget),
+  // lv_pct(100));
+  lv_obj_set_size(zmk_widget_battery_bar_obj(&battery_bar_widget), lv_pct(100),
+                  50);
+  lv_obj_align(zmk_widget_battery_bar_obj(&battery_bar_widget),
+               LV_ALIGN_TOP_MID, 0, 0);
 
-    zmk_widget_layer_roller_init(&layer_roller_widget, screen);
-    lv_obj_set_size(zmk_widget_layer_roller_obj(&layer_roller_widget), 224, 140);
-    lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_LEFT_MID, 0, -20);
+  zmk_widget_layer_roller_init(&layer_roller_widget, screen);
+  lv_obj_set_size(zmk_widget_layer_roller_obj(&layer_roller_widget),
+                  lv_pct(100), 140);
+  lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget),
+               LV_ALIGN_LEFT_MID, 0, 0);
 
-
-    return screen;
+  return screen;
 }
-
