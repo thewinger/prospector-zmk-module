46ee1bdde12fae523eebee82fc164eceda3aabf8
thewinger <winger.87@gmail.com>
2025-04-03 12:01:01 +0200

    includes font

--- boards/shields/prospector_adapter/include/fonts.h
+++ boards/shields/prospector_adapter/include/fonts.h
@@ -3,7 +3,7 @@
 #include <lvgl.h>
 
 // LV_FONT_DECLARE(SF_Compact_Text_Light_24);
-// LV_FONT_DECLARE(SF_Compact_Text_Semibold_28);
+LV_FONT_DECLARE(SF_Compact_Text_Medium_24);
 LV_FONT_DECLARE(SF_Compact_Text_Bold_32);
 // LV_FONT_DECLARE(InterDisplay_Light_40);
 // LV_FONT_DECLARE(InterDisplay_Thin_56);
